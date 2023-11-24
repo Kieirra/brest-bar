5cda11449267aa68d8bb00347c21def68dbbf924
Kieirra <velith.it@gmail>
2023-11-24 23:38:38 +0100

    feat: card component

---
+++ src/components/common/atoms/card.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import 'tailwindcss/tailwind.css';
+import Card from './card';
+
+const meta: Meta<typeof Card> = {
+    title: 'components/common/atoms/Card',
+    component: Card,
+    tags: ['autodocs']
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Card>;
+
+export const Empty: Story = {
+};
---
+++ src/components/common/atoms/card.tsx
@@ -0,0 +1,9 @@
+import tw from "tailwind-styled-components";
+
+const Card = tw.div`
+    bg-ngrey-700 
+    rounded-md 
+    p-4
+`;
+
+export default Card;
