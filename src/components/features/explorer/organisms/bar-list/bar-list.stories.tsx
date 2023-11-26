60f2a4b49129356b180356dfe51c2d1e1c9354ab
Kieirra <velith.it@gmail>
2023-11-26 02:07:08 +0100

    fix: bar-list stories

--- src/components/features/explorer/organisms/bar-list/bar-list.stories.tsx
+++ src/components/features/explorer/organisms/bar-list/bar-list.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { BarListUI } from "./bar-list";
+import { Category } from "@/../types/bar";
 
 const meta: Meta<typeof BarListUI> = {
     title: "features/explorer/organisms/BarList",
@@ -18,11 +19,13 @@ export const Fill: Story = {
                 name: "La Javanaise Bar Brest",
                 address: "40 Quai de la Douane, 29200 Brest, France",
                 website: "https://www.facebook.com/lajavanaise29200/",
+                category: Category.BAR,
             },
             {
                 name: "La Javanaise Bar Brest",
                 address: "40 Quai de la Douane, 29200 Brest, France",
                 website: "https://www.facebook.com/lajavanaise29200/",
+                category: Category.CAVE,
             },
         ],
     },
