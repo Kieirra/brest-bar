da40475d9e372f389e266223136c793ef3c8660c
Kieirra <velith.it@gmail>
2023-11-27 01:36:45 +0100

    fix: remove dead code

--- src/stores/bars-store.ts
+++ src/stores/bars-store.ts
@@ -4,7 +4,6 @@ import { Bar } from "../../types/bar";
 interface BarsStore {
     bars: Bar[];
     position: [number, number];
-    zoom: number;
     setBars: (newBars: Bar[]) => void;
     setPosition: (newPosition: [number, number]) => void;
 }
@@ -12,7 +11,6 @@ interface BarsStore {
 const useBarsStore = create<BarsStore>((set, get) => ({
     bars: [],
     position: [48.400002, -4.6],
-    zoom: 13,
     setBars: (newBars: Bar[]) => set({ bars: newBars }),
     setPosition: (newPosition: [number, number]) => set({ position: newPosition }),
 }));
